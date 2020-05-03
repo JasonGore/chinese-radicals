3681c9ec202dbbe19dcf7bf1f81a5a826fb8f894
Jason Gore <jagore@microsoft.com>
2020-05-03 13:13:51 -0700

    Basic radical start.

---
+++ src/App.tsx
@@ -0,0 +1,143 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import { cardStyles } from './Flashcard/Flashcard.styles';
+
+const radicals: Radical[] = require('./data/radicals');
+
+interface Radical {
+  number: number;
+  traditional: string;
+  english: string;
+  pinyin: string;
+  strokeCount: number;
+}
+
+const renderRadicalCard = (radical: Radical) => {
+  return <View style={styles.sectionContainer} key={radical.number}>
+    <Text style={cardStyles.traditional}>{radical.traditional}</Text>
+    <Text style={cardStyles.pinyin}>{radical.pinyin}</Text>
+    <Text style={cardStyles.english}>{radical.english}</Text>
+  </View>
+};
+
+const Home = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              {/* <Text>{JSON.stringify(radicals)}</Text> */}
+              {radicals.map((radical) => renderRadicalCard(radical))}
+            </View>
+            {/* <DefaultContent /> */}
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const DefaultContent = () => {
+  return (
+    <>
+      <Header/>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Step One</Text>
+        <Text style={styles.sectionDescription}>
+          Edit <Text style={styles.highlight}>App.tsx</Text> to change
+          this screen and then come back to see your edits.
+        </Text>
+      </View>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>See Your Changes</Text>
+        <Text style={styles.sectionDescription}>
+          <ReloadInstructions />
+        </Text>
+      </View>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Debug</Text>
+        <Text style={styles.sectionDescription}>
+          <DebugInstructions />
+        </Text>
+      </View>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Learn More</Text>
+        <Text style={styles.sectionDescription}>
+          Read the docs to discover what to do next:
+        </Text>
+      </View>
+      <LearnMoreLinks />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default Home;
---
+++ src/Flashcard/Flashcard.styles.ts
@@ -0,0 +1,40 @@
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+export const cardStyles = StyleSheet.create({
+  traditional: {
+    backgroundColor: Colors.lighter,
+    color: 'blue',
+    fontWeight: '700',
+    fontSize: 36,
+    textAlign: 'center'
+  },
+  pinyin: {
+    backgroundColor: Colors.lighter,
+    color: 'blue',
+    fontWeight: '700',
+    fontSize: 36,
+    textAlign: 'center'
+  },
+  english: {
+    backgroundColor: Colors.lighter,
+    color: 'blue',
+    fontWeight: '700',
+    fontSize: 36,
+    textAlign: 'center'
+  },
+});
